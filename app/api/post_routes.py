e364d4a60611a1a2634c1a816cf040c40ee3283e
Katherine <katherinepmai@gmail.com>
2022-12-22 17:34:13 -0600

    created post route for posts

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -25,9 +25,51 @@ def post(id):
 # post a new post
 @post_routes.route("/", methods=["POST"])
 def make_post():
+    current_user_info = User.query.get(current_user.id).to_dict()
+    current_user_id = current_user_info['id']
+
     form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate():
+        try:
+            new_post = Post(
+                body = form.data['body'],
+                user_id = current_user_id
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return new_post.to_dict(), 201
+        except Exception:
+            return {'error': 'cannot submit at this time, try again later'}
+    if form.errors:
+        return {'error': form.errors}
 
 # update a post
+@post_routes.routes("/<int:postId>", methods=["PUT"])
+def update_post(postId):
+    current_user_info = User.query.get(current_user.id).to_dict()
+    current_user_id = current_user_info['id']
+
+    # get the post by id
+    update_this_post = Post.query.get(postId)
+
+    if update_this_post:
+        if update_this_post.user_id == current_user_id:
+            #  i left off here
+            return "hello"
+        else:
+            return {'error': {
+                'message': 'Forbidden',
+                'statusCode': 403
+            }}, 403
+    else:
+        return {
+            'error': {
+                'message': 'Cannot find post. Please try again.',
+                'statusCode': 403
+            }
+        }, 404
 
 
 # delete a post
@@ -40,7 +82,7 @@ def delete_post_by_id(postId):
         if delete_post.user_id == current_user_id:
             db.session.delete(delete_post)
             db.session.commit()
-            return {'message': 'Post deleted'}
+            return {'message': 'Post successfully deleted.'}
         else:
             return {'error': {
                 'message': 'Forbidden',
