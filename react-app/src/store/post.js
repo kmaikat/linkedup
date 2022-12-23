2a54af36fcde6e6b88b20cca613c2ac0fa9869e5
Katherine <katherinepmai@gmail.com>
2022-12-22 23:26:01 -0600

    set up store with initial reducer function

---
+++ react-app/src/store/post.js
@@ -0,0 +1,14 @@
+const initialState = {}
+
+export default function reducer( state = initialState, action ){
+    // switch (action.type) {
+    //     case GET_POSTS: {
+    //         return
+    //     }
+    //     default: {
+    //         return state
+    //     }
+
+    // }
+    return null;
+}
