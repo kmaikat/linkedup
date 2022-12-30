018ff5a06d28bf5ebfd8ff019e57cd7f4cf981d7
Katherine <katherinepmai@gmail.com>
2022-12-29 23:42:29 -0600

    changed tag from div to ul

--- react-app/src/components/LandingPageNavBar.js
+++ react-app/src/components/LandingPageNavBar.js
@@ -5,32 +5,35 @@ import "../stylesheets/LandingPageNavBar.css"
 
 const LandingPageNavBar = () => {
     return (
-        <div id="landing-page-nav-bar-container">
-            <div id="landing-page-nav-bar-main-left">
-                <img id="landing-page-nav-bar-logo" src={linkedUp}></img>
-            </div>
-            <div id="landing-page-nav-bar-main-right">
-                <div id="nav-bar-right-left">
-                    <div id="repo-icon-container">
-                        <div id="repo-icon-content">
-                            <i class="fa-solid fa-compass"></i>
-                            <div>
-                                Repo
-                            </div>
-                        </div>
-                    </div>
-                    <div id="connect-icon-container">
-                        <div id="connect-icon-content">
-                            <i class="fa-solid fa-user-group"></i>
-                            <div>
-                                Connect
-                            </div>
-                        </div>
-                    </div>
+        <div id="landing-page-heading">
+
+            <div id="landing-page-nav-bar-container">
+                <div id="landing-page-nav-bar-main-left">
+                    <img id="landing-page-nav-bar-logo" src={linkedUp}></img>
                 </div>
-                <div id="nav-bar-right-actions">
-                    <a id="join-now-container" href="/sign-up">Join now</a>
-                    <a id="sign-in-container" href="/login">Sign in</a>
+                <div id="landing-page-nav-bar-main-right">
+                    <ul id="nav-bar-right-left">
+                        <li id="repo-icon-container">
+                            <a id="repo-icon-content" href="https://github.com/kmaikat/linkedIn-clone">
+                                <i class="fa-solid fa-compass"></i>
+                                <div>
+                                    Repo
+                                </div>
+                            </a>
+                        </li>
+                        <li id="connect-icon-container">
+                            <a id="connect-icon-content" href="https://www.linkedin.com/in/kpmai20/">
+                                <i class="fa-solid fa-user-group"></i>
+                                <div>
+                                    Connect
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div id="nav-bar-right-actions">
+                        <a id="join-now-container" href="/sign-up">Join now</a>
+                        <a id="sign-in-container" href="/login">Sign in</a>
+                    </div>
                 </div>
             </div>
         </div>
