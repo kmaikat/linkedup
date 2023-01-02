733e9715450481b10ca5fbacbd4ec9120a66cae5
Katherine <katherinepmai@gmail.com>
2023-01-02 01:50:09 -0600

    Styled post css

--- react-app/src/components/CreatePostsComponents/CreatePost.js
+++ react-app/src/components/CreatePostsComponents/CreatePost.js
@@ -16,12 +16,12 @@ function CreatePost({ setShowModal }) {
             <div id="create-post-heading-exit-container">
                 <div id="create-post-heading-exit-content">
 
-                <div id="create-post-subtitle">
-                    Create a post
-                </div>
-                <div id='create-post-exit-button'>
-                    <i onClick={() => setShowModal(false)} className="fa-solid fa-x"></i>
-                </div>
+                    <div id="create-post-subtitle">
+                        Create a post
+                    </div>
+                    <div id='create-post-exit-button'>
+                        <i onClick={() => setShowModal(false)} className="fa-solid fa-x"></i>
+                    </div>
                 </div>
             </div>
             <div id="create-post-user-info-container">
@@ -33,14 +33,18 @@ function CreatePost({ setShowModal }) {
                 </div>
             </div>
             <div id="form-container">
-                <form id="body-form">
+                <form id="body-form" onSubmit={(e) => { e.preventDefault(); console.log(body) }}>
                     <div id="create-post-textarea">
                         <textarea name='body' placeholder='What do you want to talk about?' value={body} onChange={updateBody} />
                     </div>
                 </form>
             </div>
             <div id="create-post-footer-container">
-                <button>Post</button>
+                <div id="empty-div">
+                </div>
+                <div id="post-container">
+                    <button id="create-post-form-can-submit" form="body-form" type="submit" disabled={body.length < 1}>Post</button>
+                </div>
             </div>
         </div>
     )
