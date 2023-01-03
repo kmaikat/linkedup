f54de723238a8497dc6a51712ff1b842fc6f1ad4
Katherine <katherinepmai@gmail.com>
2023-01-02 20:10:12 -0600

    Outline for posts on the home page

--- react-app/src/components/AppHome.js
+++ react-app/src/components/AppHome.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
-import "../stylesheets/AppHome.css"
 import CreatePostModal from "./CreatePostsComponents/CreatePostModal"
 import NavBar from "./NavBar"
+import threeDots from "../assets/three-dots.svg"
+import noPP from "../assets/no-pp.png";
+import "../stylesheets/AppHome.css"
 
 const AppHome = () => {
     const posts = useSelector(state => state.posts?.posts);
@@ -17,7 +19,31 @@ const AppHome = () => {
                     <CreatePostModal />
                     <ul>
                         {posts?.map(post => {
-                            return (<li>
+                            return (<li className="app-home-post">
+                                <div id="app-home-post-heading-container">
+                                    <div id="app-home-heading-left-container">
+                                        <div id="create-post-user-info-icon">
+                                            <img id='no-pp' src={noPP} />
+                                        </div>
+                                        <div id="app-home-post-heading-name">
+                                            <div className="app-home-post-user-heading">
+                                                name HERE
+                                            </div>
+                                            <div className="app-home-post-user-heading">
+                                                title HERE
+                                            </div>
+                                            <div className="app-home-post-user-heading">
+                                                date posted here
+                                            </div>
+
+
+
+                                        </div>
+                                    </div>
+                                    <div id="app-home-heading-right-container-options">
+                                        <img src={threeDots} />
+                                    </div>
+                                </div>
                                 {post.body}
                             </li>)
                         }).reverse()}
