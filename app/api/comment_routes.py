7b39568ae3778118918df70b9c933bf5f1c45c78
Katherine <katherinepmai@gmail.com>
2023-01-04 16:17:44 -0600

    Created delete route for comments

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -69,3 +69,26 @@ def update_comment(comment_id):
 
 
 # delete comment
+@comment_routes.route('/<int:comment_id>', methods=["DELETE"])
+def delete_comment(comment_id):
+    current_user_info = User.query.get(current_user.id).to_dict()
+    current_user_id = current_user_info['id']
+
+    delete_comment = Comment.query.get(comment_id)
+
+    if delete_comment:
+        if delete_comment.user_id == current_user_id:
+            db.session.delete(delete_comment)
+            db.session.commit()
+            return {'message': 'Comment successfully deleted.'}
+        else:
+            return {'error': {
+                'message': 'Forbidden',
+                'statusCode': 403
+            }}, 403
+            
+    else:
+        return {'error': {
+            'message': 'Cannot find comment you were looking for',
+            'statusCode': 404
+        }}, 404
