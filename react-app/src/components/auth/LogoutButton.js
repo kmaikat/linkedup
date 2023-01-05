ae3a6328e9a2be0eea048385928248a94ef3c206
Katherine <katherinepmai@gmail.com>
2023-01-05 01:01:02 -0600

    Fix styling for logout section, add npm package for time ago

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import "../../stylesheets/AppNavBar.css"
+
+
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e)
     await dispatch(logout());
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <p style={{ cursor: "pointer" }} id='sign-out-button' onClick={onLogout}>Sign Out</p>;
 };
 
 export default LogoutButton;
