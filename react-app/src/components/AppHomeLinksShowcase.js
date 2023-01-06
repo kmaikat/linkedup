26d0fcbb3b7cbcf3505fe8062eb0bb29a8dc50e7
Katherine <katherinepmai@gmail.com>
2023-01-05 20:36:54 -0600

    app home left side fully styled with links

--- react-app/src/components/AppHomeLinksShowcase.js
+++ react-app/src/components/AppHomeLinksShowcase.js
@@ -1,6 +1,16 @@
+import "../stylesheets/AppHomeProfileShowcase.css"
+
 const AppHomeLinksShowcase = () => {
     return (
-        "LINKS GO HERE"
+        <div id="app-home-link-showcase-outer-container">
+            <div id="app-home-link-showcase-inner-container">
+                <ul className="app-home-links">
+                    <p>Let's Connect!</p>
+                    <li><a href="https://github.com/kmaikat" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/kpmai20/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
