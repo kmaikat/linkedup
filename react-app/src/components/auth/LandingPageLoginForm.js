ff1857814fa2baff468f248631678883dfd38ddc
Katherine <katherinepmai@gmail.com>
2023-01-06 21:36:16 -0600

    error handling on login page finished

--- react-app/src/components/auth/LandingPageLoginForm.js
+++ react-app/src/components/auth/LandingPageLoginForm.js
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import "../../stylesheets/LandingPageLoginForm.css"
 
 const LandingPageLoginForm = () => {
-  const [errors, setErrors] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [errors, setErrors] = useState({});
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,15 +15,37 @@ const LandingPageLoginForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  const regex = RegExp(
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+  );
+
   const onLogin = async (e) => {
     e.preventDefault();
+    setHasSubmitted(true);
     const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
+
+
+    const errorObj = {};
+    if (!email) errorObj.email = "Please enter your email adress."
+    if (!email.trim().match(regex)) errorObj.email = "Please enter a valid email address"
+    // if (data.email) errorObj.email = "Couldn’t find a LinkedUp account associated with this email. Please try again."
+    if (!password) errorObj.password = "Please enter your password."
+    if (password.length < 8) errorObj.password = "Password must be 8 characters or more."
+    // if (data.password) errorObj.password = "That's not the right password. Please try again."
+    if (!email && !password) errorObj.both = "Please fill out both fields."
+
+    if (Object.keys(errorObj).length !== 0) {
+      setErrors(errorObj)
+      return;
     }
+
+    // if (data) {
+    //   setErrors(data);
+    // }
   };
 
   const demoLogin = async (e) => {
+    e.stopPropagation();
     e.preventDefault();
     const data = await dispatch(login("demo@aa.io", "password"));
   }
@@ -35,49 +58,51 @@ const LandingPageLoginForm = () => {
     setPassword(e.target.value);
   };
 
+  useEffect(() => {
+    if (hasSubmitted === false) return
+  }, [email, password])
+
   if (user) {
     return <Redirect to='/feed' />;
   }
 
+  console.log(errors)
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div className='landing-page-login-input'
-        ref={emailFieldContainer}
-        onClick={() => {
-          emailFieldContainer.current.children.email.focus()
-        }}
-      >
-        <label htmlFor='email' className={email.length > 0 ? "landing-page-login-label-filled" : ""}>Email</label>
-        <input
-          name='email'
-          type='text'
-
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div className='landing-page-login-input' ref={passwordFieldContainer}
-        onClick={() => {
-          passwordFieldContainer.current.children.password.focus()
-        }}
-      >
-        <label htmlFor='password' className={password.length > 0 ? "landing-page-login-label-filled" : ""}>Password</label>
-        <input
-          name='password'
-          type='password'
-
-          value={password}
-          onChange={updatePassword}
-        />
-      </div>
+    <>
+      <h2 className="input-error-test-landing-page-error-title">{errors.both ? errors.both : errors.email || errors.password}</h2>
+      <form onSubmit={onLogin} onKeyDown={e => e.key === "Enter" ? onLogin(e) : undefined}>
+        <div className={`landing-page-login-input ${errors.email ? "input-error" : ""}`}
+          ref={emailFieldContainer}
+          onClick={() => {
+            emailFieldContainer.current.children.email.focus()
+          }}
+        >
+          <label htmlFor='email' className={email.length > 0 ? "landing-page-login-label-filled" : ""}>Email</label>
+          <input
+            name='email'
+            type='text'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className={`landing-page-login-input ${errors.password ? "input-error" : ""}`}
+          ref={passwordFieldContainer}
+          onClick={() => {
+            passwordFieldContainer.current.children.password.focus()
+          }}
+        >
+          <label htmlFor='password' className={password.length > 0 ? "landing-page-login-label-filled" : ""}>Password</label>
+          <input
+            name='password'
+            type='password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+      </form>
       <button id="landing-page-demo-user" onClick={demoLogin}> Sign in as Demo User</button>
-      <button type='submit' id="landing-page-login-sign-in">Sign in</button>
-    </form>
+      <button onClick={onLogin} id="landing-page-login-sign-in">Sign in</button>
+    </>
   );
 };
 
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -24,7 +24,7 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
       return;
     }
-  
+
     dispatch(setUser(data));
   }
 }
@@ -40,8 +40,8 @@ export const login = (email, password) => async (dispatch) => {
       password
     })
   });
-  
-  
+
+
   if (response.ok) {
     const data = await response.json();
     dispatch(setUser(data))
@@ -82,7 +82,7 @@ export const signUp = (username, email, password) => async (dispatch) => {
       password,
     }),
   });
-  
+
   if (response.ok) {
     const data = await response.json();
     dispatch(setUser(data))
@@ -90,6 +90,7 @@ export const signUp = (username, email, password) => async (dispatch) => {
   } else if (response.status < 500) {
     const data = await response.json();
     if (data.errors) {
+      console.log(data.errors)
       return data.errors;
     }
   } else {
