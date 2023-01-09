dcfdb491dbbc8bddc4d42b5bc83c58393fa47bb6
Katherine <katherinepmai@gmail.com>
2023-01-08 21:56:04 -0600

    Added styling to the login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -68,60 +68,73 @@ const LoginForm = () => {
 
   return (
 
-    <>
-      <div>
-        <img src={logo} alt="linkedup-logo" />
-      </div>
-      <div>
-        <div>Sign In</div>
-        <div>Stay updated on your professional world</div>
-      </div>
-      <form class="login-page-form" onSubmit={onLogin} onKeyDown={e => e.key === "Enter" ? onLogin(e) : undefined}>
+    <div id='login-form-page-outer-container'>
+      <div id='login-form-page-inner-container'>
+        <nav id="login-page-navbar">
+          <a href='/'>
+            <img id='login-page-logo-icon' src={logo} alt="linkedup-logo" />
+          </a>
+        </nav>
+        <div id='body'>
+          <div id="login-page-body-container">
 
-        <div className='login-page-input-container'>
-          <div id="login-page-input" className={`landing-page-login-input ${errors.email ? "input-error" : ""}`}
-            ref={emailFieldContainer}
-            onClick={() => {
-              emailFieldContainer.current.children.email.focus()
-            }}
-          >
-            <label htmlFor='email' className={email.length > 0 ? "landing-page-login-label-filled" : ""}>Email</label>
-            <input
-              name='email'
-              type='text'
-              value={email}
-              onChange={updateEmail}
-            />
-          </div>
-          <p className="input-error-text">{errors.email}</p>
-        </div>
+            <div id='form-body-header'>
+              <div>Sign in</div>
+              <div>Stay updated on your professional world</div>
+            </div>
+            <form class="login-page-form" onSubmit={onLogin} onKeyDown={e => e.key === "Enter" ? onLogin(e) : undefined}>
 
-        <div className='login-page-input-container'>
-          <div id="login-page-input" className={`landing-page-login-input ${errors.password ? "input-error" : ""}`}
-            ref={passwordFieldContainer}
-            onClick={() => {
-              passwordFieldContainer.current.children.password.focus()
-            }}
-          >
-            <label htmlFor='password' className={password.length > 0 ? "landing-page-login-label-filled" : ""}>Password</label>
-            <input
-              name='password'
-              type='password'
-              value={password}
-              onChange={updatePassword}
-            />
+              <div className='login-page-input-container'>
+                <div id="login-page-input" className={`landing-page-login-input ${errors.email ? "input-error" : ""}`}
+                  ref={emailFieldContainer}
+                  onClick={() => {
+                    emailFieldContainer.current.children.email.focus()
+                  }}
+                >
+                  <label htmlFor='email' className={email.length > 0 ? "landing-page-login-label-filled" : ""}>Email</label>
+                  <input
+                    name='email'
+                    type='text'
+                    value={email}
+                    onChange={updateEmail}
+                  />
+                </div>
+                <p className="input-error-text">{errors.email}</p>
+              </div>
+
+              <div className='login-page-input-container'>
+                <div id="login-page-input" className={`landing-page-login-input ${errors.password ? "input-error" : ""}`}
+                  ref={passwordFieldContainer}
+                  onClick={() => {
+                    passwordFieldContainer.current.children.password.focus()
+                  }}
+                >
+                  <label htmlFor='password' className={password.length > 0 ? "landing-page-login-label-filled" : ""}>Password</label>
+                  <input
+                    name='password'
+                    type='password'
+                    value={password}
+                    onChange={updatePassword}
+                  />
+                </div>
+                <p className="input-error-text">{errors.password}</p>
+              </div>
+            </form>
+            <button type='submit' id="landing-page-login-sign-in" onClick={onLogin}>Sign In</button>
+            <div id='login-form-button-sparator'>
+              <hr className='login-line'></hr>
+              <p id='or-separator'>or</p>
+              <hr className='login-line'></hr>
+            </div>
+            <button type='submit' id="login-page-login-sign-in" className='demo-sign-in' onClick={demoLogin}><i className="fa-solid fa-user"></i>Sign in as Demo User</button>
           </div>
-          <p className="input-error-text">{errors.password}</p>
+          <div id='direct-to-sign-up-container'>
+            <p>New to LinkedUp?</p>
+            <a href='/sign-up'>Join now</a>
+          </div>
         </div>
-      </form>
-      <button type='submit' id="landing-page-login-sign-in" onClick={onLogin}>Sign In</button>
-      <div id='login-form-button-sparator'>
-        <hr className='login-line'></hr>
-        <span className='or-button-separator'></span>or
-        <hr className='login-line'></hr>
       </div>
-      <button type='submit' id="login-page-login-sign-in" className='demo-sign-in' onClick={demoLogin}><i className="fa-solid fa-user"></i>Sign in as Demo User</button>
-    </>
+    </div>
 
   );
 };
