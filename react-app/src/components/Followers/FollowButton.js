7eda801239a2ed4f9b7c057991ed4c7e635c74b6
Katherine <katherinepmai@gmail.com>
2023-02-05 10:27:31 -0600

    Added css to following button

--- react-app/src/components/Followers/FollowButton.js
+++ react-app/src/components/Followers/FollowButton.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react"
-
+import "../../stylesheets/FollowButton.css"
 const FollowButton = () => {
     const [isFollowed, setIsFollowed] = useState(false)
-
     
+
     return (
-        <div>
-            <div>
-                <i className="fa-regular fa-plus"></i>
+        <>
+            <div className="not-followed">
+                <i id="follow-plus-icon" className="fa-regular fa-plus"></i>
                 Follow
             </div>
-        </div>
+        </>
     )
 }
 
