f64b2bcd37c19bb7629255b7c2c7f37c196b4204
Katherine <katherinepmai@gmail.com>
2023-03-03 12:08:34 -0600

    heading for messages styled

--- react-app/src/components/Messages/MessagesPreviews.js
+++ react-app/src/components/Messages/MessagesPreviews.js
@@ -1,7 +1,18 @@
 import "../../stylesheets/MessagePreviews.css"
 const MessagesPreviews = () => {
      return (
-        <div className="hello-test">hello</div>
+        <div className="messages-preview-outer-container">
+            <div className="messages-preview-heading-outer">
+
+            <div className="messages-preview-heading">
+                <div>Messaging</div>
+                <p>
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </p>
+            </div>
+            </div>
+
+        </div>
      )
 }
 
