913478ef2b13fb861969049368b100722f4b42e1
Katherine <katherinepmai@gmail.com>
2023-03-07 20:17:19 -0600

    set up thunks

--- react-app/src/components/Followers/FollowButton.js
+++ react-app/src/components/Followers/FollowButton.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { updateUser } from "../../store/session"
+import { updateUser, updateUserThunk } from "../../store/session"
 import "../../stylesheets/FollowButton.css"
 
 const FollowButton = ({ post }) => {
@@ -15,9 +15,25 @@ const FollowButton = ({ post }) => {
     // 1. send the post's user information to the following route with a fetch request
     // 2. bring that information back and store it in our state
     // 3. let the user know whether they are following or not (button)
-    const handleFollow =() => {
+    const handleFollow = async(event) => {
         console.log("following... ")
+        event.preventDefault();
+        // fetch to the route
+        const response = await fetch(`/api/users/${post.user_id}/following`,{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(post.user_id)
+        })
+
+        if (response.ok) {
+            dispatch(updateUserThunk())
+        } else {
+            const errors = await response.json();
+            return errors;
+        };
+
     }
+
     const handleUnfollow =() => {
         console.log("unfollowing...")
     }
