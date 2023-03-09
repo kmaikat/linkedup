57c6cb713c7040036aa7b0a18c1022d186535c2b
Katherine <katherinepmai@gmail.com>
2023-03-08 21:12:18 -0600

    re-initialized sockets

--- react-app/src/components/Messages/MessageRoom.js
+++ react-app/src/components/Messages/MessageRoom.js
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { io } from 'socket.io-client';
+import "../../stylesheets/MessageRoom.css";
 
-import "../../stylesheets/MessageRoom.css"
-
-
+let socket;
 
 const MessageRoom = () => {
     const [chatInput, setChatInput] = useState("");
     const [messages, setMessages] = useState([]);
     const user = useSelector(state => state.session.user)
 
+    useEffect(() => {
 
+        // create websocket
+        socket = io();
+
+        // listen for chat events
+        socket.on("chat", (chat) => {
+            // when we recieve a chat, add it into our messages array in state
+            setMessages(messages => [...messages, chat])
+        })
+
+        // when component unmounts, disconnect
+        return (() => {
+            socket.disconnect()
+        })
+    }, [])
 
     const updateChatInput = (e) => {
         setChatInput(e.target.value)
     };
 
-    const sendChat = (e) => {
-        e.preventDefault()
-
-        setChatInput("")
-    }
+    // const sendChat = (e) => {
+    //     e.preventDefault()
+    //     // emit a message
+    //     socket.emit("chat", { sender_id: user.username, room_id: 1, message: chatInput });
+    //     // clear the input field after the message is sent
+    //     setChatInput("")
+    // }
 
     return (
         <div>
