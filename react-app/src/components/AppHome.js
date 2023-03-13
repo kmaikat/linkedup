5e0143df9574c9f79366d7b20b6a9fa20151c6f7
Katherine <katherinepmai@gmail.com>
2023-03-13 00:31:51 -0500

    Removed unused function

--- react-app/src/components/AppHome.js
+++ react-app/src/components/AppHome.js
@@ -11,10 +11,6 @@ import AppHomeAboutShowcase from "./AppHomeAboutShowcase"
 const AppHome = () => {
     const posts = useSelector(state => Object.values(state.posts));
 
-
-    // const handleEditToggle = async (post) => {
-    //     const errors = dispatch(editPostThunk(post))
-    // }
     return (
         <div className="app-home-outer-container">
             <NavBar />
