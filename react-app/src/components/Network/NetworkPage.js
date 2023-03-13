97c9cb2f204f7890f854b9993420d37cacea64df
Katherine <katherinepmai@gmail.com>
2023-03-12 23:24:04 -0500

    can switch tabs in network page

--- react-app/src/components/Network/NetworkPage.js
+++ react-app/src/components/Network/NetworkPage.js
@@ -2,10 +2,19 @@ import NavBar from "../NavBar"
 import "../../stylesheets/NetworkPage.css"
 import Footer from "../Footer"
 import { useSelector } from "react-redux"
+import { useState } from "react"
 
 const NetworkPage = () => {
     const followers = useSelector(state => state.session.user.followers)
     const following = useSelector(state => state.session.user.following)
+    const [selected, setSelected] = useState("")
+
+    const selectFollowing= () => {
+        setSelected("following")
+    }
+    const selectFollowers= () => {
+        setSelected("followers")
+    }
 
     console.log(Object.keys(followers).length)
     return (
@@ -15,27 +24,30 @@ const NetworkPage = () => {
                 <div className="network-left-view">
                     <div className="network-left-heading">Manage my network</div>
                     <ul className="network-links">
-                        <li className="manage-network-links">
+                        <li className="manage-network-links" onClick={selectFollowing}>
                             <div>
                                 <i class="fa-solid fa-user"></i>
                                 Following
                             </div>
-                            {/* add amount here */}
                             {Object.keys(following).length}
                         </li>
-                        <li className="manage-network-links">
+                        <li className="manage-network-links" onClick={selectFollowers}>
                             <div>
                             <i class="fa-solid fa-user"></i>
                                 Followers
                             </div>
-                            {/* add amount here */}
                             {Object.keys(followers).length}
                         </li>
                     </ul>
                     <div className="network-footer-separator"></div>
                     <Footer></Footer>
                 </div>
-                <div className="network-right-view">right</div>
+                <div className="network-right-view">
+                    {selected == "following" &&
+                    <div>heeeeee</div>}
+                    {selected == "followers" &&
+                    <div>hi</div>}
+                </div>
             </div>
         </div>
     )
